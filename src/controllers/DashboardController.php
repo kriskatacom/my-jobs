6e8a825d5e226f57c44ff59e0f5cc2f7638e9a42
kriskatacom <info@kriskata.com>
2025-08-22 14:37:50 +0300

    feat: Display user and category counts on the dashboard

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -2,14 +2,37 @@
 
 namespace App\Controllers;
 
+use Core\Database;
+
+use App\Repositories\UserRepository;
+use App\Repositories\CategoryRepository;
+
 require_once dirname(__DIR__) . '/Helpers/languages.php';
 
 use Core\View;
 
 class DashboardController
 {
+    private $db;
+    private $categoryRepository;
+    private $userRepository;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+        $this->categoryRepository = new CategoryRepository($this->db);
+        $this->userRepository = new UserRepository($this->db);
+    }
+    
     public function index()
     {
-        View::render('dashboard/index', ['title' => __('dashboard')]);
+        $categoryCount = $this->categoryRepository->getCategoryCount();
+        $userCount = $this->userRepository->getUserCount();
+        
+        View::render('dashboard/index', [
+            'title' => __('dashboard'),
+            'categoryCount' => $categoryCount,
+            'userCount' => $userCount,
+        ]);
     }
 }
--- src/views/dashboard/index.php
+++ src/views/dashboard/index.php
@@ -5,7 +5,23 @@
 <main>
     <div class="flex gap-5">
         <?php require dirname(__DIR__) . '/layout/dashboard-navbar.php'; ?>
-        <h1 class="my-5 text-2xl text-center"><?= __('dashboard') ?></h1>
+        <div class="w-full">
+            <h1 class="my-5 text-2xl"><?= __('dashboard') ?></h1>
+            <ul class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                <li class="border border-gray-200 rounded-md shadow shadow-gray-200 overflow-hidden">
+                    <a href="/dashboard/users" class="bg-white block py-5 text-center text-2xl">
+                        <?= __('users') ?>
+                        <div><?= htmlspecialchars($userCount) ?></div>
+                    </a>
+                </li>
+                <li class="border border-gray-200 rounded-md shadow shadow-gray-200 overflow-hidden">
+                    <a href="/dashboard/categories" class="bg-white block py-5 text-center text-2xl">
+                        <?= __('categories') ?>
+                        <div><?= htmlspecialchars($categoryCount) ?></div>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 
 </main>
